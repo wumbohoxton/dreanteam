0910ee79802ff9e97bc8d0f27f6bd390e0ae6f5b
wumbohoxton <dylanhoang55@gmail.com>
2026-02-01 20:32:15 -0800

    created page and functions, PageRange class and initialization, little bit for initializing Table needs heavily worked on

--- lstore/page.py
+++ lstore/page.py
@@ -1,14 +1,38 @@
+CAPACITY = 4096
+MANDATORY_COLUMNS = 4
+MAX_BASE_PAGES = 16
 
 class Page:
 
     def __init__(self):
         self.num_records = 0
-        self.data = bytearray(4096)
+        self.data = bytearray()
+        self.page_number = 0
+        self.page_size = 0
 
-    def has_capacity(self):
-        pass
+    def has_capacity(self, size):
+        return self.page_size + size < CAPACITY
 
-    def write(self, value):
-        self.num_records += 1
-        pass
+    def write(self, value: bytes):
+        if self.has_capacity(len(value)):
+            value.to_bytes()
+            self.data[self.page_size:self.page_size + len(value)] = value
+            self.page_size += len(value)
+            return True
+        return False
+    
+    # todo create a read function, needs to be able to account for different data types fml
+        
+class PageRange:
+
+    def __init__(self, num_columns): # initialize 16 base pages indexed at 0
+        self.base_pages = []
+        for base_page in range(0, MAX_BASE_PAGES - 1): # make 16 base pages
+            for page in range(0, (num_columns + MANDATORY_COLUMNS - 1)): 
+                self.base_pages[base_page][page] = Page(page) # create a page for each column
+            
+        self.tail_pages = []
+        for page in range(0, num_columns + MANDATORY_COLUMNS - 1): #create one tail page
+            self.tail_pages[0][page] = Page(page)
+
 
--- lstore/table.py
+++ lstore/table.py
@@ -1,10 +1,12 @@
 from lstore.index import Index
 from time import time
+from lstore.page import Page
 
 INDIRECTION_COLUMN = 0
 RID_COLUMN = 1
 TIMESTAMP_COLUMN = 2
 SCHEMA_ENCODING_COLUMN = 3
+
 
 
 class Record:
@@ -24,11 +26,23 @@ class Table:
     def __init__(self, name, num_columns, key):
         self.name = name
         self.key = key
-        self.num_columns = num_columns
-        self.page_directory = {}
+        self.num_columns = num_columns # + 4 for rid, indirection, schema, timestamping
+        self.page_directory = {} # key: column, RID --> value: page_range, page, page_offset
+        self.page_ranges = []
         self.index = Index(self)
         self.merge_threshold_pages = 50  # The threshold to trigger a merge
+
+
         pass
+        # initalizing table
+        self.page_ranges.append(PageRange(self.num_columns)) # create initial page range
+        self.page_ranges[0]
+
+        
+    # def page_range_init(self):
+    #     for column in 
+
+    #     pass
 
     def __merge(self):
         print("merge is happening")
