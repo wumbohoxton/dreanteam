54c424592d29b20fcb2c2124df77ebf92b5aa78f
rhchaudri <rhchaudri@ucdavis.edu>
2026-02-07 23:49:25 -0800

    cut some

--- lstore/query.py
+++ lstore/query.py
@@ -1,6 +1,7 @@
 from lstore.table import Table, Record
 from lstore.index import Index
 
+LATEST_VERSION = 0
 
 class Query:
     """
@@ -21,26 +22,26 @@ class Query:
     # Return False if record doesn't exist or is locked due to 2PL
     """
     def delete(self, primary_key):
-        #read a record
-        #use index to locate rid
-        rid = self.table.index.locate( self.table.key, primary_key)
+        # #read a record
+        # #use index to locate rid
+        # rid = self.table.index.locate( self.table.key, primary_key)
         
-        #if NA skip rest of the steps
-        if rid == None:
-            return False
+        # #if NA skip rest of the steps
+        # if rid == None:
+        #     return False
 
-        try:
-            #address = self.table.page_directory[rid]
-            #delete address
-            del self.table.page_directory[rid]
-            #delete primary key
-            del self.table.index[primary_key]
-            return True
+        # try:
+        #     #address = self.table.page_directory[rid]
+        #     #delete address
+        #     del self.table.page_directory[rid]
+        #     #delete primary key
+        #     del self.table.index[primary_key]
+        #     return True
         
-        #if locked
-        except: 
-            return False
-
+        # #if locked
+        # except: 
+        #     return False
+        self.table.delete_record(primary_key)
     
     
     """
@@ -49,24 +50,24 @@ class Query:
     # Returns False if insert fails for whatever reason
     """
     def insert(self, *columns):
-        #variables
-        schema_encoding = '0' * self.table.num_columns
-        primary_key = columns[self.table.key]
-        rid = len(self.table.page_directory)
-    
-        #checks for duplicates//should be unique
-        if self.table.index.locate(self.table.key, primary_key) != None:
-            return False
+        # #variables
+        # schema_encoding = '0' * self.table.num_columns
+        # primary_key = columns[self.table.key]
+        # rid = len(self.table.page_directory)
+        RIDs = self.table.index.locate(self.table.key, columns[self.table.key]) 
+        if len(RIDs) >= 1:
+             return False
 
-        try: 
-            #insert address to directory to get to the columns
-            self.table.page_directory[rid] = columns
-            #insert for rid key mapping
-            self.table.index[primary_key] = rid
-            return True
+        # try: 
+        #     #insert address to directory to get to the columns
+        #     self.table.page_directory[rid] = columns # tuple of column and RID
+        #     #insert for rid key mapping
+        #     self.table.index[primary_key] = rid
+        #     return True
         
-        except:
-            return False       
+        # except:
+        #     return False  
+        return self.table.insert_new_record(columns)     
         
         
 
@@ -80,28 +81,29 @@ class Query:
     # Assume that select will never be called on a key that doesn't exist
     """
     def select(self, search_key, search_key_index, projected_columns_index):
-        #read
+        #introduce some sort of rab bit hunting through the tail records, 
+            # as well as checking what values we have gathered already
         #rid key map
-        rid = self.table.index.locate(search_key_index, search_key)
+        # get one RID
+        # get RID of base record, then access indirection and get tail record, 
+            #   get specified column data we want
+        rids = self.table.index.locate(search_key_index, search_key) 
         
-        try:
-            #get full record, not just the rid to get the cols
-            record = self.table.page_directory[rid]
-            #change projected_col_index to fit record format from 1 | 0 values
-            #Record(rid, key, cols) format wanted
-            return_columns = []
-            
-            for i in range(len(projected_columns_index)):
-                #if 1 then we want that column from record
-                if projected_columns_index[i] == 1:
-                    return_columns.append(record[i])
-
-            #return a list!! of Record ojs
-            return [Record(rid, search_key, return_columns)]
+        #can cut //not needed since select will never be called on key that doesnt exist
+        #if len(rids) == 0:
+        #    return False
         
-        #if locked
-        except:
-            return False
+        rid = rids[0]
+        return_columns = []
+        
+        for i in range(len(projected_columns_index)):
+            if projected_columns_index[i] == 1:
+                return_columns.append(self.table.rabbit_hunt(i, search_key, LATEST_VERSION))
+            #can the else be ignored
+            #else:
+            #    return_columns.append(None)
+        #return a list of Record ojs
+        return [Record(rid, search_key, return_columns)]
     
 
 
@@ -116,34 +118,31 @@ class Query:
     # Assume that select will never be called on a key that doesn't exist
     """
     def select_version(self, search_key, search_key_index, projected_columns_index, relative_version):
-        #only change is record to record versions then choose version and continue
-
-        #read
+        #introduce some sort of rab bit hunting through the tail records, 
+            # as well as checking what values we have gathered already
         #rid key map
-        rid = self.table.index.locate(search_key_index, search_key)
+        # get one RID
+        # get RID of base record, 
+            #  then access indirection and get tail record, get specified column data we want
+        rids = self.table.index.locate(search_key_index, search_key)
         
-        try:
-            #get record's versions so it's not just one version
-            record_versions = self.table.page_directory[rid]
-            #record w relative version
-            return_version = record_versions[relative_version]
-            #change projected_col_index to fit record format from 1 | 0 values
-            #Record(rid, key, cols) format wanted
-            return_columns = []
-            
-            for i in range(len(projected_columns_index)):
-                #if 1 then we want that column from record
-                if projected_columns_index[i] == 1:
-                    return_columns.append(return_version[i])
+        #select assumption means not needed
+        #if len(rids) == 0:
+        #    return False
+        
+        rid = rids[0]
+        return_columns = []
+        
+        for i in range(len(projected_columns_index)):
+            if projected_columns_index[i] == 1:
+                return_columns.append(self.table.rabbit_hunt(i, search_key, relative_version))
+            #try without else
+            #else:
+            #   return_columns.append(None)
+        #return a list of Record ojs
+        return [Record(rid, search_key, return_columns)]
 
-            #return a list!! of Record ojs
-            return [Record(rid, search_key, return_columns)]
-       
-        #if locked
-        except:
-            return False
-
-####
+#############
 
 
 
@@ -153,7 +152,36 @@ class Query:
     # Returns False if no records exist with given key or if the target record cannot be accessed due to 2PL locking
     """
     def update(self, primary_key, *columns):
-        pass
+        # # locating our record using the primary key, index to locate faster
+        # rid = self.table.index.locate(self.table.key, primary_key)
+
+        # # if the record doesn't exist, the update doesn't go through
+        # if rid is None:
+        #     return False
+        
+        # try:
+        #     # reading the current record directly
+        #     record = self.table.page_directory[rid]
+
+        #     # starting the new version as a copy of the most recent
+        #     new_record = list(record)
+
+        #     # updating column by column
+        #     for i in range(len(columns)):
+        #         if columns[i] is not None:
+        #             # to not change the primary key
+        #             if i == self.table.key and columns[i] != primary_key:
+        #                 return False
+        #             new_record[i] = columns[i]
+
+        #     # appending the newest version, but not overwriting the old ones
+        #     self.table.page_directory[rid] = tuple(new_record)
+
+        #     return True
+        # except:
+        #     return False
+
+        return self.table.update_record(primary_key, columns)
 
     
     """
@@ -165,6 +193,24 @@ class Query:
     # Returns False if no record exists in the given range
     """
     def sum(self, start_range, end_range, aggregate_column_index):
+        total = 0   # starting at 0 count
+        found = False   # making sure there is at least 1 valid record found
+
+        try:
+            # going through all of the keys in range
+            for key in range(start_range, end_range + 1):
+
+                # adding values from the most recent versions
+                to_add = self.table.rabbit_hunt(aggregate_column_index, key, LATEST_VERSION)
+                if to_add is not None:
+                    total += to_add
+                found = True
+
+            # return false if no records are found
+            return total if found else False
+        except:
+            return False
+
         pass
 
     
@@ -178,7 +224,26 @@ class Query:
     # Returns False if no record exists in the given range
     """
     def sum_version(self, start_range, end_range, aggregate_column_index, relative_version):
-        pass
+        # no versions will exist if page_directory stores only 1 tuple per RID
+
+        total = 0   # starting at 0 count
+        found = False   # making sure there is at least 1 valid record found
+
+        try:
+            # going through all of the keys in range
+            for key in range(start_range, end_range + 1):
+
+                # adding values from the most recent versions
+
+                to_add = self.table.rabbit_hunt(aggregate_column_index, key, relative_version)
+                if to_add is not None:
+                    total += to_add
+                found = True
+            # return false if no records are found
+            return total if found else False
+        except:
+            return False
+
 
     
     """
@@ -192,8 +257,12 @@ class Query:
     def increment(self, key, column):
         r = self.select(key, self.table.key, [1] * self.table.num_columns)[0]
         if r is not False:
+            # creating an update list
             updated_columns = [None] * self.table.num_columns
+
+            # incrementing the specific column
             updated_columns[column] = r[column] + 1
-            u = self.update(key, *updated_columns)
-            return u
+
+            # returning and applying the updated columns
+            return self.update(key, *updated_columns)
         return False
